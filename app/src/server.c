f319b26f63dd461cc2abb0860400f514cf91a0b5
iainen <iain@iainen.com>
2022-09-30 15:51:56 +0800

    remover unused function

--- app/src/server.c
+++ app/src/server.c
@@ -19,49 +19,6 @@
 #define SC_SERVER_PATH_DEFAULT PREFIX "/share/scrcpy/" SC_SERVER_FILENAME
 #define SC_DEVICE_SERVER_PATH "/data/local/tmp/scrcpy-server.jar"
 
-static char *
-get_server_path(void) {
-#ifdef __WINDOWS__
-    const wchar_t *server_path_env = _wgetenv(L"SCRCPY_SERVER_PATH");
-#else
-    const char *server_path_env = getenv("SCRCPY_SERVER_PATH");
-#endif
-    if (server_path_env) {
-        // if the envvar is set, use it
-#ifdef __WINDOWS__
-        char *server_path = sc_str_from_wchars(server_path_env);
-#else
-        char *server_path = strdup(server_path_env);
-#endif
-        if (!server_path) {
-            LOG_OOM();
-            return NULL;
-        }
-        LOGD("Using SCRCPY_SERVER_PATH: %s", server_path);
-        return server_path;
-    }
-
-#ifndef PORTABLE
-    LOGD("Using server: " SC_SERVER_PATH_DEFAULT);
-    char *server_path = strdup(SC_SERVER_PATH_DEFAULT);
-    if (!server_path) {
-        LOG_OOM();
-        return NULL;
-    }
-#else
-    char *server_path = sc_file_get_local_path(SC_SERVER_FILENAME);
-    if (!server_path) {
-        LOGE("Could not get local file path, "
-             "using " SC_SERVER_FILENAME " from current directory");
-        return strdup(SC_SERVER_FILENAME);
-    }
-
-    LOGD("Using server (portable): %s", server_path);
-#endif
-
-    return server_path;
-}
-
 static void
 sc_server_params_destroy(struct sc_server_params *params) {
     // The server stores a copy of the params provided by the user
@@ -104,41 +61,6 @@ error:
 }
 
 static bool
-push_server(struct sc_intr *intr, const char *serial) {
-    char *server_path = get_server_path();
-    if (!server_path) {
-        return false;
-    }
-    if (!sc_file_is_regular(server_path)) {
-        LOGE("'%s' does not exist or is not a regular file\n", server_path);
-        free(server_path);
-        return false;
-    }
-    bool ok = sc_adb_push(intr, serial, server_path, SC_DEVICE_SERVER_PATH, 0);
-    free(server_path);
-    return ok;
-}
-
-static const char *
-log_level_to_server_string(enum sc_log_level level) {
-    switch (level) {
-        case SC_LOG_LEVEL_VERBOSE:
-            return "verbose";
-        case SC_LOG_LEVEL_DEBUG:
-            return "debug";
-        case SC_LOG_LEVEL_INFO:
-            return "info";
-        case SC_LOG_LEVEL_WARN:
-            return "warn";
-        case SC_LOG_LEVEL_ERROR:
-            return "error";
-        default:
-            assert(!"unexpected log level");
-            return "(unknown)";
-    }
-}
-
-static bool
 sc_server_sleep(struct sc_server *server, sc_tick deadline) {
     sc_mutex_lock(&server->mutex);
     bool timed_out = false;
@@ -150,133 +72,6 @@ sc_server_sleep(struct sc_server *server, sc_tick deadline) {
     sc_mutex_unlock(&server->mutex);
 
     return !stopped;
-}
-
-static sc_pid
-execute_server(struct sc_server *server,
-               const struct sc_server_params *params) {
-    sc_pid pid = SC_PROCESS_NONE;
-
-    const char *serial = server->serial;
-    assert(serial);
-
-    const char *cmd[128];
-    unsigned count = 0;
-    cmd[count++] = sc_adb_get_executable();
-    cmd[count++] = "-s";
-    cmd[count++] = serial;
-    cmd[count++] = "shell";
-    cmd[count++] = "CLASSPATH=" SC_DEVICE_SERVER_PATH;
-    cmd[count++] = "app_process";
-
-#ifdef SERVER_DEBUGGER
-# define SERVER_DEBUGGER_PORT "5005"
-    cmd[count++] =
-# ifdef SERVER_DEBUGGER_METHOD_NEW
-        /* Android 9 and above */
-        "-XjdwpProvider:internal -XjdwpOptions:transport=dt_socket,suspend=y,"
-        "server=y,address="
-# else
-        /* Android 8 and below */
-        "-agentlib:jdwp=transport=dt_socket,suspend=y,server=y,address="
-# endif
-            SERVER_DEBUGGER_PORT;
-#endif
-    cmd[count++] = "/"; // unused
-    cmd[count++] = "com.genymobile.scrcpy.Server";
-    cmd[count++] = SCRCPY_VERSION;
-
-    unsigned dyn_idx = count; // from there, the strings are allocated
-#define ADD_PARAM(fmt, ...) { \
-        char *p = (char *) &cmd[count]; \
-        if (asprintf(&p, fmt, ## __VA_ARGS__) == -1) { \
-            goto end; \
-        } \
-        cmd[count++] = p; \
-    }
-
-    ADD_PARAM("log_level=%s", log_level_to_server_string(params->log_level));
-    ADD_PARAM("bit_rate=%" PRIu32, params->bit_rate);
-
-    if (params->max_size) {
-        ADD_PARAM("max_size=%" PRIu16, params->max_size);
-    }
-    if (params->max_fps) {
-        ADD_PARAM("max_fps=%" PRIu16, params->max_fps);
-    }
-    if (params->lock_video_orientation != SC_LOCK_VIDEO_ORIENTATION_UNLOCKED) {
-        ADD_PARAM("lock_video_orientation=%" PRIi8,
-                  params->lock_video_orientation);
-    }
-    if (server->tunnel.forward) {
-        ADD_PARAM("tunnel_forward=true");
-    }
-    if (params->crop) {
-        ADD_PARAM("crop=%s", params->crop);
-    }
-    if (!params->control) {
-        // By default, control is true
-        ADD_PARAM("control=false");
-    }
-    if (params->display_id) {
-        ADD_PARAM("display_id=%" PRIu32, params->display_id);
-    }
-    if (params->show_touches) {
-        ADD_PARAM("show_touches=true");
-    }
-    if (params->stay_awake) {
-        ADD_PARAM("stay_awake=true");
-    }
-    if (params->codec_options) {
-        ADD_PARAM("codec_options=%s", params->codec_options);
-    }
-    if (params->encoder_name) {
-        ADD_PARAM("encoder_name=%s", params->encoder_name);
-    }
-    if (params->power_off_on_close) {
-        ADD_PARAM("power_off_on_close=true");
-    }
-    if (!params->clipboard_autosync) {
-        // By default, clipboard_autosync is true
-        ADD_PARAM("clipboard_autosync=false");
-    }
-    if (!params->downsize_on_error) {
-        // By default, downsize_on_error is true
-        ADD_PARAM("downsize_on_error=false");
-    }
-    if (!params->cleanup) {
-        // By default, cleanup is true
-        ADD_PARAM("cleanup=false");
-    }
-    if (!params->power_on) {
-        // By default, power_on is true
-        ADD_PARAM("power_on=false");
-    }
-
-#undef ADD_PARAM
-
-    cmd[count++] = NULL;
-
-#ifdef SERVER_DEBUGGER
-    LOGI("Server debugger waiting for a client on device port "
-         SERVER_DEBUGGER_PORT "...");
-    // From the computer, run
-    //     adb forward tcp:5005 tcp:5005
-    // Then, from Android Studio: Run > Debug > Edit configurations...
-    // On the left, click on '+', "Remote", with:
-    //     Host: localhost
-    //     Port: 5005
-    // Then click on "Debug"
-#endif
-    // Inherit both stdout and stderr (all server logs are printed to stdout)
-    pid = sc_adb_execute(cmd, 0);
-
-end:
-    for (unsigned i = dyn_idx; i < count; ++i) {
-        free((char *) cmd[i]);
-    }
-
-    return pid;
 }
 
 static bool
@@ -482,63 +277,6 @@ fail:
     return false;
 }
 
-static void
-sc_server_on_terminated(void *userdata) {
-    struct sc_server *server = userdata;
-
-    // If the server process dies before connecting to the server socket,
-    // then the client will be stuck forever on accept(). To avoid the problem,
-    // wake up the accept() call (or any other) when the server dies, like on
-    // stop() (it is safe to call interrupt() twice).
-    sc_intr_interrupt(&server->intr);
-
-    server->cbs->on_disconnected(server, server->cbs_userdata);
-
-    LOGD("Server terminated");
-}
-
-static bool
-is_tcpip_mode_enabled(struct sc_server *server, const char *serial) {
-    struct sc_intr *intr = &server->intr;
-
-    char *current_port =
-        sc_adb_getprop(intr, serial, "service.adb.tcp.port", SC_ADB_SILENT);
-    if (!current_port) {
-        return false;
-    }
-
-    // Is the device is listening on TCP on port 5555?
-    bool enabled = !strcmp("5555", current_port);
-    free(current_port);
-    return enabled;
-}
-
-static bool
-wait_tcpip_mode_enabled(struct sc_server *server, const char *serial,
-                        unsigned attempts, sc_tick delay) {
-    if (is_tcpip_mode_enabled(server, serial)) {
-        LOGI("TCP/IP mode enabled");
-        return true;
-    }
-
-    // Only print this log if TCP/IP is not enabled
-    LOGI("Waiting for TCP/IP mode enabled...");
-
-    do {
-        sc_tick deadline = sc_tick_now() + delay;
-        if (!sc_server_sleep(server, deadline)) {
-            LOGI("TCP/IP mode waiting interrupted");
-            return false;
-        }
-
-        if (is_tcpip_mode_enabled(server, serial)) {
-            LOGI("TCP/IP mode enabled");
-            return true;
-        }
-    } while (--attempts);
-    return false;
-}
-
 char *
 append_port_5555(const char *ip) {
     size_t len = strlen(ip);
@@ -556,109 +294,9 @@ append_port_5555(const char *ip) {
     return ip_port;
 }
 
-static char *
-sc_server_switch_to_tcpip(struct sc_server *server, const char *serial) {
-    assert(serial);
-
-    struct sc_intr *intr = &server->intr;
-
-    LOGI("Switching device %s to TCP/IP...", serial);
-
-    char *ip = sc_adb_get_device_ip(intr, serial, 0);
-    if (!ip) {
-        LOGE("Device IP not found");
-        return NULL;
-    }
-
-    char *ip_port = append_port_5555(ip);
-    free(ip);
-    if (!ip_port) {
-        return NULL;
-    }
-
-    bool tcp_mode = is_tcpip_mode_enabled(server, serial);
-
-    if (!tcp_mode) {
-        bool ok = sc_adb_tcpip(intr, serial, 5555, SC_ADB_NO_STDOUT);
-        if (!ok) {
-            LOGE("Could not restart adbd in TCP/IP mode");
-            goto error;
-        }
-
-        unsigned attempts = 40;
-        sc_tick delay = SC_TICK_FROM_MS(250);
-        ok = wait_tcpip_mode_enabled(server, serial, attempts, delay);
-        if (!ok) {
-            goto error;
-        }
-    }
-
-    return ip_port;
-
-error:
-    free(ip_port);
-    return NULL;
-}
-
-static bool
-sc_server_connect_to_tcpip(struct sc_server *server, const char *ip_port) {
-    struct sc_intr *intr = &server->intr;
-
-    // Error expected if not connected, do not report any error
-    sc_adb_disconnect(intr, ip_port, SC_ADB_SILENT);
-
-    LOGI("Connecting to %s...", ip_port);
-
-    bool ok = sc_adb_connect(intr, ip_port, 0);
-    if (!ok) {
-        LOGE("Could not connect to %s", ip_port);
-        return false;
-    }
-
-    LOGI("Connected to %s", ip_port);
-    return true;
-}
-
-static bool
-sc_server_configure_tcpip_known_address(struct sc_server *server,
-                                        const char *addr) {
-    // Append ":5555" if no port is present
-    bool contains_port = strchr(addr, ':');
-    char *ip_port = contains_port ? strdup(addr) : append_port_5555(addr);
-    if (!ip_port) {
-        LOG_OOM();
-        return false;
-    }
-
-    server->serial = ip_port;
-    return sc_server_connect_to_tcpip(server, ip_port);
-}
-
-static bool
-sc_server_configure_tcpip_unknown_address(struct sc_server *server,
-                                          const char *serial) {
-    bool is_already_tcpip =
-        sc_adb_device_get_type(serial) == SC_ADB_DEVICE_TYPE_TCPIP;
-    if (is_already_tcpip) {
-        // Nothing to do
-        LOGI("Device already connected via TCP/IP: %s", serial);
-        return true;
-    }
-
-    char *ip_port = sc_server_switch_to_tcpip(server, serial);
-    if (!ip_port) {
-        return false;
-    }
-
-    server->serial = ip_port;
-    return sc_server_connect_to_tcpip(server, ip_port);
-}
-
 static int
 run_server(void *data) {
     struct sc_server *server = data;
-
-    const struct sc_server_params *params = &server->params;
 
     // Execute "adb start-server" before "adb devices" so that daemon starting
     // output/errors is correctly printed in the console ("adb devices" output
@@ -691,7 +329,6 @@ run_server(void *data) {
 
     // Give some delay for the server to terminate properly
 #define WATCHDOG_DELAY SC_TICK_FROM_SEC(1)
-    sc_tick deadline = sc_tick_now() + WATCHDOG_DELAY;
     return 0;
 
 error_connection_failed:
